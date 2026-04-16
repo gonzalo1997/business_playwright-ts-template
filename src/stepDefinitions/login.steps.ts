3975038dd20b1d861e0443571f10c605f25dcb94
gonzalo1997 <gonza.magallanes1997@gmail.com>
2026-04-16 01:19:15 -0300

    fix name missmatching

--- src/stepDefinitions/login.steps.ts
+++ src/stepDefinitions/login.steps.ts
@@ -8,7 +8,7 @@ let page: Page;         // explicitly typed
 let loginPage: LoginPage;
 const user = createValidUser();
 
-Given('the user opens the LinkedIn login page', async function () {
+Given('the user opens the SauceDemo login page', async function () {
   browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   page = await context.newPage();
@@ -20,7 +20,7 @@ When('they enter valid email and password', async function () {
     await loginPage.login(user.username, user.password);
 });
 
-Then('they should be redirected to the LinkedIn feed', async function () {
+Then('they should be redirected to the SauceDemo feed', async function () {
   await expect(page).toHaveURL(process.env.BASE_URL!);
   await browser.close();
 });
--- tests/features/login.feature
+++ tests/features/login.feature
@@ -1,6 +1,6 @@
 Feature: LinkedIn Login
 
   Scenario: User logs in successfully
-    Given the user opens the LinkedIn login page
+    Given the user opens the SauceDemo login page
     When they enter valid email and password
-    Then they should be redirected to the LinkedIn feed
+    Then they should be redirected to the SauceDemo feed
